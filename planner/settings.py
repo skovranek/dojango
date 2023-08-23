b4c71af74f91cd2dbdaa1f155a9c92cb8db90ecd
skovranek <mattjskov@gmail.com>
2023-08-23 00:18:20 -0400

    add pytz to settings

--- planner/settings.py
+++ planner/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import dj_database_url
 import os
+from pytz import timezone
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'EDT'
+TIME_ZONE = timezone('US/Eastern')
 
 USE_I18N = True
 
