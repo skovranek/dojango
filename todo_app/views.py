e0bb4180a9c1efcf633ae6c672d36f28634a5905
skovranek <59619403+skovranek@users.noreply.github.com>
2023-10-01 11:14:50 -0400

    try redirect to introduction

--- todo_app/views.py
+++ todo_app/views.py
@@ -33,7 +33,6 @@ class HomeView(generic.ListView):
 
     def get_context_data(self):
         if self.request.session.get('user_id') is not None:
-            self.request.session.get('user_id', str(uuid.uuid4()))
             context = super().get_context_data()
             questions = Question.objects.filter(date__date=timezone.localtime(timezone.now())).filter(user=self.request.session['user_id']).order_by('priority')
             if len(questions) > 0:
@@ -41,8 +40,6 @@ class HomeView(generic.ListView):
             if Motto.objects.filter(user=self.request.session['user_id']).exists():
                 context['motto'] = Motto.objects.filter(user=self.request.session['user_id']).latest()
             return context
-        else:
-            self.request.session['user_id'] = str(uuid.uuid4())
 
 
 def introduction(request):
